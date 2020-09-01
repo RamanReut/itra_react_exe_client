73800f69e80e62c3f09bf9dec5b477d69ddc6323
Raman Reut <r.reut@itransition.com>
2020-09-01 08:29:18 +0300

    Add expand animation to search input on hover; Extract search input into separate component; Hide menu button for large and middle monitor

---
+++ src/features/layout/SearchInput.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import SearchIcon from '@material-ui/icons/Search'
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {},
+    item: {
+        [theme.breakpoints.up('sm')]: {
+            width: '75%',
+            '&:hover, &:focus-within': {
+                width: '100%',
+            },
+            transitionTimingFunction: theme.transitions.easing.easeIn,
+            transitionDuration: `${theme.transitions.duration.standard}ms`,
+            transitionProperty: 'width',
+        },
+    },
+    input: {
+        '& .MuiOutlinedInput-root': {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+}));
+
+export interface SearchInputProps {
+    text: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function SearchInput({
+    text,
+    onChange,
+}: SearchInputProps ) {
+    const classes = useStyles();
+
+    return (
+        <Grid 
+            container
+            direction='row-reverse'
+            className={classes.root}
+        >
+            <Grid 
+                item
+                className={classes.item}
+            >
+                <TextField
+                    fullWidth
+                    className={classes.input}
+                    data-testid='header-searchInput'
+                    variant='outlined'
+                    type='search'
+                    color='primary'
+                    size='small'
+                    placeholder='Quick search...'
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position='start'>
+                                <SearchIcon></SearchIcon>
+                            </InputAdornment>                       
+                        ),
+                    }}
+                    value={text}
+                    onChange={onChange}
+                ></TextField> 
+            </Grid>
+        </Grid>
+    );
+}
