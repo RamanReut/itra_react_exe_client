94f93cca77818ea56e1acaa0a2c93cc248170ecd
Raman Reut <r.reut@itransition.com>
2020-09-02 12:07:13 +0300

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Layout } from './features/layout';
 import { DataTable } from './features/dataTable';
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import classnames from 'classnames'
+import { Switch, Route } from 'react-router-dom'
+import { NotFound } from './features/notFound'
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
@@ -18,7 +20,14 @@ function App() {
     return (
         <div className={classnames('App', classes.root)}>
             <Layout>
-                <DataTable></DataTable>
+                <Switch>
+                    <Route path='/data'>
+                        <DataTable></DataTable>
+                    </Route>
+                    <Route path='*'>
+                        <NotFound></NotFound>
+                    </Route>
+                </Switch>
             </Layout>      
         </div>
   );
