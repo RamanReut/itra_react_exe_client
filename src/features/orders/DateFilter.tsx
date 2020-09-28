5e6e87af494c3de1aa9c4a705c5d99a7c186d8cc
Raman Reut <r.reut@itransition.com>
2020-09-28 10:33:36 +0300

    finish integrate data range filter

--- src/features/orders/DateFilter.tsx
+++ src/features/orders/DateFilter.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { types } from './reducer'
 import { Column } from 'material-table'
 import { DateRangePicker, DateRange } from '../../share/dateRangePicker'
 
 export interface OrderDateFilterProps {
     columnDef: Column<types.Row>;
-    onFilterChanged: (rowId: string, value: any) => void
+    onFilterChanged: (rowId: string, value: any) => void;
+}
+
+interface FieldId {
+    tableData: {
+        id: number;
+    }
 }
 
 export default function OrderDateFilter({
@@ -13,6 +19,12 @@ export default function OrderDateFilter({
     onFilterChanged,
 }: OrderDateFilterProps) {
     const [range, setRange] = useState<DateRange>({ start: null, end: null });
+
+    useEffect(() => {
+        if (columnDef.field) {
+            onFilterChanged(`${(columnDef as FieldId).tableData.id}`, range);
+        }
+    }, [columnDef, onFilterChanged, range])
 
     return (
         <DateRangePicker
--- src/features/orders/dateFilters.ts
+++ src/features/orders/dateFilters.ts
@@ -8,14 +8,14 @@ export function dateRangeFilter(
     rowData: types.Row,
     columnDef: Column<types.Row>,
 ) {
-    const {
-        start = THE_START_DATE,
-        end = new Date(),
-    } = filter;
-    const curDate = new Date(rowData[columnDef.field as types.Columns]);
+    if (filter.start || filter.end) {
+        const start = filter.start ? filter.start : THE_START_DATE;
+        const end = filter.end ? filter.end : new Date();
+        const curDate = new Date(rowData[columnDef.field as types.Columns]);
 
-    if (curDate < start && curDate > end) {
-        return false;
+        if (curDate < start || curDate > end) {
+            return false;
+        }
     }
     return true;
 }
