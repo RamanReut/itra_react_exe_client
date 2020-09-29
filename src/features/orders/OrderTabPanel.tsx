6f3bfc5e45cdbc19ba6d1d9b28a97523d9bb8033
Raman Reut <r.reut@itransition.com>
2020-09-29 16:46:34 +0300

    Remove unnecessary import useMemo

--- src/features/orders/OrderTabPanel.tsx
+++ src/features/orders/OrderTabPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import { types } from './reducer'
