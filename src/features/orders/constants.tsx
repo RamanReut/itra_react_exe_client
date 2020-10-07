9bfff002f68320fd49d42577ac40eebfe89cbac6
Raman Reut <r.reut@itransition.com>
2020-10-07 08:19:17 +0300

    Delete unused MAP_STATUS_ID_TO_TEXT

--- src/features/orders/constants.tsx
+++
@@ -1,8 +0,0 @@
-import { MapNumberToString } from './reducer/types';
-
-export const MAP_STATUS_ID_TO_TEXT: MapNumberToString = {
-    1: 'Ordered',
-    2: 'Processing',
-    3: 'Cancel',
-    4: 'Complete',
-};
