23a95f06e5158e93442186e00254aa903ab4b611
Raman Reut <r.reut@itransition.com>
2020-10-07 05:48:11 +0300

    Add translation to CustomerInfo

--- src/features/orders/CustomerInfo.tsx
+++ src/features/orders/CustomerInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import DetailTextWrapper from './DetailLongTextWrapper'
+import { useTranslation } from 'react-i18next'
 
 export interface CustomerInfoProps {
     customer_name: string;
@@ -9,18 +10,20 @@ export interface CustomerInfoProps {
 }
 
 export default function CustomerInfo(props: CustomerInfoProps) {
+    const { t } = useTranslation('orders');
+
     return (
         <Grid 
             container
             spacing={1}
         >
-            <DetailTextWrapper title='Name'>
+            <DetailTextWrapper title={t('columns.customer_name')}>
                 {props.customer_name}
             </DetailTextWrapper>
-            <DetailTextWrapper title='Email'>
+            <DetailTextWrapper title={t('columns.email')}>
                 {props.email}
             </DetailTextWrapper>
-            <DetailTextWrapper title='Address'>
+            <DetailTextWrapper title={t('columns.address')}>
                 {props.address}
             </DetailTextWrapper>
         </Grid>
