d8228735d05638df80546bdd81893c66ccfec6d3
Raman Reut <r.reut@itransition.com>
2020-10-08 09:29:13 +0300

    Add primary color to darkTheme

--- src/features/theme/darkTheme.ts
+++ src/features/theme/darkTheme.ts
@@ -5,5 +5,9 @@ export const darkTheme = createMuiTheme({
     ...basicTheme,
     palette: {
         type: 'dark',
+        primary: {
+            light: "rgba(129, 92, 237, 1)",
+            main: "rgba(33, 21, 84, 1)",
+        },
     }
 })
